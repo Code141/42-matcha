7699384ebaf43aabda1f5277472e6d6638e52d84
Fiona DELSING <fdelsing@e3r6p12.42.fr>
2018-11-15 19:48:21 +0100

    working on wrappers - problem with syntaxe

--- app/controllers/sql_test.php
+++ app/controllers/sql_test.php
@@ -4,18 +4,8 @@ class c_sql_test extends c_controller
 {
 	public function main($params = NULL)
 	{
-		$this->model = new m_model($this->pdo);
-		$this->data['sql'] = $this->model
-			->select(array("*"),"user")
-			->where("id", "=" ,"1")
-;//			->and("username", "=", "fiofy");
-		echo($this->model->sql. "<br>");
-		$this->data['executed'] = $this->pdo->execute_pdo()->fetchAll();
-/*		
-		$this->data['sql'] = 
-			$this->pdo
-			->select(array("firstname"),"user")
-			->left_join("");
- */		$this->core->set_view("sql_test", "main");
+		$model = $this->load->model("sql_test");
+		$this->data['executed'] = $model->matches();
+		$this->core->set_view("sql_test", "main");
 	}
 }
---
+++ app/models/sql_test.php
@@ -0,0 +1,37 @@
+<?php
+
+class m_sql_test extends m_wrapper
+{
+	public $sql;
+
+	public function test()
+	{
+		$this->select(array("user" => "username"),"user")->where("user","id", "=" , "1");
+		$this->db->sql = $this->sql; 
+		$this->data['sql'] = $this->db->sql;
+		var_dump($this->data['sql']);
+		echo "<br>";
+		$result = $this->db->execute_pdo()->fetch(PDO::FETCH_ASSOC);
+		return ($result);
+	}
+	public function matches()
+	{
+		$this
+			->select(array("user" => "username"),"user")
+			->join("LEFT", "user_orientation", "user", "user_id", "id")
+			->join("LEFT", "user_gender", "user", "user_id", "id")
+			->join("LEFT", "user_gender_identity", "user", "user_id", "id")
+			->where("user_orientation", "id_gender", "=" , "1")
+			->and("user_orientation", "id_gender_identity", "=" , "1")
+			->and("user","id", "=" , "1");
+			;
+		$this->db->sql = $this->sql; 
+		$this->data['sql'] = $this->db->sql;
+		var_dump($this->data['sql']);
+		echo "<br>";
+		var_dump($this->db->bind_param);
+		$result = $this->db->execute_pdo()->fetch(PDO::FETCH_ASSOC);
+		return ($result);
+	}
+
+}
