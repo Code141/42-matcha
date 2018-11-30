a51070701cbe92e9bf2c4f86f7b8d4d2377ba312
Fiona DELSING <fdelsing@e3r6p6.42.fr>
2018-11-30 14:40:44 +0100

    profil page is done

--- app/controllers/profil.php
+++ app/controllers/profil.php
@@ -4,13 +4,12 @@ class c_profil extends c_controller
 {
 	public function main($params = NULL)
 	{
-		$param = 1;
 		$user = $this->module_loader->session()->controller->user_loggued();
-		$this->req = $this->load->model("profil")
-			->fetch_user($param, $user['id'])
-				->execute()
-			->fetchAll();
-
+		if ($params)
+			$this->data['profil'] = $this->load->model("profil")
+				->create_profil($params[0], $user['id']);
+		else
+			$this->data['profil'] = $user;
 		$this->core->set_view("profil", "main");
 	}
 }
