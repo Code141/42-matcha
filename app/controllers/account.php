f2dc561b1152c615caa4719423a7e033900c5f59
Fiona DELSING <fdelsing@e3r6p6.42.fr>
2018-12-05 19:52:17 +0100

    working on account editing validation

--- app/controllers/account.php
+++ app/controllers/account.php
@@ -10,4 +10,33 @@ class c_account extends c_controller
 		$this->data['all_gender_id'] = $model->fetch_all_from("gender_identity");
 		$this->core->set_view("account", "main");
 	}
+
+	private function recursive_in_array($haystack, $needle)
+	{
+		if (is_array($haystack))
+		{
+			foreach ($haystack as $entry)
+				if (is_array($entry) && in_array($needle, $entry))
+				{
+					$this->recursive_in_array($entry, $needle);
+					return TRUE;
+				}
+		}
+		return FALSE;
+	}
+
+	public function add_match_pref()
+	{
+		$model = $this->load->model("account");
+		$all_genders = $model->fetch_all_from("gender");
+		$all_gender_id = $model->fetch_all_from("gender_identity");
+		if ($_POST['gender'] == "" || $_POST['gender_identity'] == "")
+			return FALSE;
+		if (($_POST['gender'] !== 'NULL' && !$this->recursive_in_array($all_genders, $_POST['gender'])) ||
+			($_POST['gender_identity'] !== 'NULL' && !$this->recursive_in_array($all_gender_id, $_POST['gender_identity'])))
+		{
+			return FALSE;
+		}
+		echo "ok";
+	}	
 }
