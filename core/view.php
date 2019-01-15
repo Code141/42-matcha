0f72f337cba20197b68255a0eef0bae36bd6c847
Code141 <lambinet.141@gmail.com>
2019-01-15 02:53:41 +0100

    glyph

--- core/view.php
+++ core/view.php
@@ -57,6 +57,7 @@ class v_view
 		$this->data = $this->protect_html_injection($this->data);
 		$basic_css[] = 'reset';
 		$basic_css[] = 'style';
+		$basic_css[] = 'glyph';
 		$this->css_files = array_merge($basic_css, $this->css_files);
 		$this->js_files[] = 'xhr';
 		$this->load_html('layout/' . $this->layout);
